798fc0169356550b1e3be3b3a355a299b6f1833d
Peter Hallam <peter@peterhallam.com>
2022-01-29 10:37:19 -0800

    cleanup

--- Program.cs
+++ Program.cs
@@ -3,45 +3,33 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 
-namespace wordle
-{
-    class Program
-    {
-        static bool matchesTemplate(string template, string value)
-        {
-            if (template.Length != value.Length)
-            {
+namespace wordle {
+    class Program {
+        static bool MatchesTemplate(string template, string value) {
+            if (template.Length != value.Length) {
                 return false;
             }
-            foreach (var pair in Enumerable.Zip(template, value))
-            {
-                if (pair.First != pair.Second && pair.First != '_')
-                {
+            foreach (var pair in Enumerable.Zip(template, value)) {
+                if (pair.First != pair.Second && pair.First != '_') {
                     return false;
                 }
             }
             return true;
         }
 
-        static bool containsAny(HashSet<char> values, string value)
-        {
+        static bool ContainsAny(HashSet<char> values, string value) {
             return value.Any(ch => values.Contains(ch));
         }
 
-        static bool containsAll(List<char> values, string value)
-        {
+        static bool ContainsAll(List<char> values, string value) {
             return values.All(value.Contains);
         }
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-
+        static void Main(string[] args) {
             var words = File.ReadAllLines(args[0]);
             var template = args[1];
             var excludes = new HashSet<char>(args[2]);
             var includes = args.Length >= 4 ? new List<char>(args[3]) : new List<char>();
-            foreach (var word in words.Where(word => matchesTemplate(template, word) && !containsAny(excludes, word) && containsAll(includes, word)))
-            {
+            foreach (var word in words.Where(word => MatchesTemplate(template, word) && !ContainsAny(excludes, word) && ContainsAll(includes, word))) {
                 Console.WriteLine(word);
             }
         }
