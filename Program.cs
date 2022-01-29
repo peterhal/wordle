d074e52d7f6f6df7a4c7ebd9f0dc684b7d4828cf
Peter Hallam <peter@peterhallam.com>
2022-01-29 10:48:07 -0800

    usage

--- Program.cs
+++ Program.cs
@@ -9,12 +9,7 @@ namespace wordle {
             if (template.Length != value.Length) {
                 return false;
             }
-            foreach (var pair in Enumerable.Zip(template, value)) {
-                if (pair.First != pair.Second && pair.First != '_') {
-                    return false;
-                }
-            }
-            return true;
+            return Enumerable.Zip(template, value).All(pair => pair.First == pair.Second || pair.First == '_');
         }
 
         static bool ContainsAny(HashSet<char> values, string value) {
@@ -24,7 +19,12 @@ namespace wordle {
         static bool ContainsAll(List<char> values, string value) {
             return values.All(value.Contains);
         }
-        static void Main(string[] args) {
+        static int Main(string[] args) {
+            if (args.Length != 4) {
+                Console.Error.WriteLine("Usage: wordle <dictionary> <template> <excludes> <includes>");
+                return 1;
+            }
+
             var words = File.ReadAllLines(args[0]);
             var template = args[1];
             var excludes = new HashSet<char>(args[2]);
@@ -32,6 +32,7 @@ namespace wordle {
             foreach (var word in words.Where(word => MatchesTemplate(template, word) && !ContainsAny(excludes, word) && ContainsAll(includes, word))) {
                 Console.WriteLine(word);
             }
+            return 0;
         }
     }
 }
