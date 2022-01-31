c0922c4ba3c71a00688934e1925085b5e29ce2ea
Peter Hallam <peter@peterhallam.com>
2022-01-31 02:10:42 -0800

    Interactive mode

--- Program.cs
+++ Program.cs
@@ -54,23 +54,111 @@ namespace wordle {
             return result;
         }
 
+        static bool IsValidColorChar(char ch) {
+            switch (ch) {
+                case 'b': case 'y': case 'g': return true;
+                default: return false;
+            }
+        }
+
         static int Main(string[] args) {
-            if (args.Length != 4) {
+            var words = File.ReadAllLines(args[0]).ToList();
+
+            if (args.Length == 4) {
+                var template = args[1];
+                var compiledTemplate = compileTemplate(template);
+                var excludes = new HashSet<char>(args[2]);
+                var includes = args.Length >= 4 ? new List<char>(args[3]) : new List<char>();
+                var results = GetResults(words, compiledTemplate, excludes, includes);
+                PrintResults(results);
+            } else if (args.Length == 1) {
+                // interactive mode
+                var guesses = new List<Tuple<string, string>>();
+                int? wordLength = null;
+                var allValidGuesses = new List<string>();
+                var possibleWords = new List<string>();
+                do {
+                    Console.WriteLine("Enter your guessed word, followed by the colors returned for your guess.");
+                    Console.WriteLine("Valid colors are b - black; y - yellow; g - green.");
+                    Console.WriteLine("> ");
+                    var input = Console.ReadLine();
+                    var inputWords = input.Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                    if (inputWords.Length != 2) {
+                        Console.WriteLine($"Invalid input: Input must have exactly 2 entries; found {inputWords.Length}.");
+                        continue;
+                    } else if (inputWords[0] is var guess && inputWords[1] is var colors && guess.Length != colors.Length) {
+                        Console.WriteLine($"Invalid input: Each entry must be same length. Got {guess.Length} and {colors.Length}");
+                        continue;
+                    } else {
+                        if (!wordLength.HasValue) {
+                            wordLength = guess.Length;
+                            allValidGuesses = words.Where(word => word.Length == wordLength).ToList();
+                            possibleWords = allValidGuesses.ToList();
+                        }
+
+                        if (wordLength != guess.Length) {
+                            Console.WriteLine($"Invalid guess: Each guess must have the same length. This guess has {guess.Length} chars, previous guess had {wordLength} chars.");
+                            continue;
+                        } else if (!allValidGuesses.Contains(guess)) {
+                            Console.WriteLine($"Invalid guess: Guess word {guess} is not a valid word in the dictionary.");
+                            continue;
+                        } else if (!colors.All(IsValidColorChar)) {
+                            Console.WriteLine($"Invalid color: Second word in input must contain only valid colors (b, y, g). Found '{new String(colors.Where(ch => !IsValidColorChar(ch)).ToArray())}'");
+                            continue;
+                        } else {
+                            // valid input!
+                            CompileInteractiveInput(guess, colors, out List<Predicate<char>> compiledTemplate, out HashSet<char> excludes, out List<char> includes);
+                            possibleWords = GetResults(possibleWords, compiledTemplate, excludes, includes);
+                            PrintResults(possibleWords);
+                        }
+
+                    }
+
+                } while (!wordLength.HasValue || possibleWords.Count > 1);
+
+            } else {
                 Console.Error.WriteLine("Usage: wordle <dictionary> <template> <excludes> <includes>");
                 return 1;
             }
+            return 0;
+        }
 
-            var words = File.ReadAllLines(args[0]);
-            var template = args[1];
-            var compiledTemplate = compileTemplate(template);
-            var excludes = new HashSet<char>(args[2]);
-            var includes = args.Length >= 4 ? new List<char>(args[3]) : new List<char>();
-            var results = words.Where(word => MatchesCompiledTemplate(compiledTemplate, word) && !ContainsAny(excludes, word) && ContainsAll(includes, word)).ToArray();
+        private static void CompileInteractiveInput(string guess, string colors, out List<Predicate<char>> compiledTemplate, out HashSet<char> excludes, out List<char> includes) {
+            compiledTemplate = new List<Predicate<char>>();
+            excludes = new HashSet<char>();
+            includes = new List<char>();
+            foreach (var pair in Enumerable.Zip(guess, colors)) {
+                var ch = pair.First;
+                switch (pair.Second) {
+                    case 'b':
+                        excludes.Add(ch);
+                        compiledTemplate.Add(c => true);
+                        break;
+                    case 'y':
+                        compiledTemplate.Add(c => c != ch);
+                        // TODO: This condition does not correctly handle the case where
+                        // there's also a green 'ch' in the guess
+                        // Correct condition is that chars in 'b' guesses must contain ch.
+                        includes.Add(ch);
+                        break;
+                    case 'g':
+                        compiledTemplate.Add(c => c == ch);
+                        break;
+                    default:
+                        throw new Exception("Unexpected color");
+                }
+            }
+        }
+
+        private static void PrintResults(List<string> results) {
             foreach (var word in results) {
                 Console.WriteLine(word);
             }
-            Console.WriteLine(results.Length);
-            return 0;
+            Console.WriteLine(results.Count);
+        }
+
+        private static List<string> GetResults(List<string> words, List<Predicate<char>> compiledTemplate, HashSet<char> excludes, List<char> includes) {
+            return words.Where(word => MatchesCompiledTemplate(compiledTemplate, word) && !ContainsAny(excludes, word) && ContainsAll(includes, word)).ToList();
         }
     }
 }
--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 A simple wordle helper written in C#.
 
+## Single Guess Mode
+
+Enter the word dictionary file name, the template of your guess, chars to exclude and chars to include.
+The solver prints all possible matched words.
+
 Parameters:
 
 - file name of dictionary with 1 word per line
-- template: use '_' for unknown characters, use letters for known (green) characters
+- template: use '_' for unknown characters, use letters for known (green) characters, use '[chars]' for yellow chars.
 - list of chars that are NOT in the target (grey characters)
 - list of chars that are known to be in the target, but unknown location (yellow characters)
 
@@ -14,7 +19,7 @@ All parameters are required, use an empty string ("") on the command line if nee
 Example Usage:
 
 ```
-bin/Debug/net5.0/wordle.exe words_alpha.txt s__a_ tinpycf ru
+bin/Debug/net5.0/wordle.exe words_alpha.txt s[r]_a_ tinpycf ru
 ```
 
 Yields:
@@ -25,4 +30,53 @@ sugar
 surah
 sural
 suras
+```
+
+## Interactive Mode
+
+Enter a series of your guess/result pairs. After each pair the solver will print the remaining possible words.
+The 'guess' is a the word you guessed. Must be contained in the dictionary.
+The 'result' is a string of chars representing the colors indicated by your guess in wordle. 
+
+Where the chars to color map is:
+   b - black
+   y - yellow
+   g - green
+
+The program exits when there is only 1 possible word left.
+
+Here's an example interactive session:
+
+```
+Enter your guessed word, followed by the colors returned for your guess.
+Valid colors are b - black; y - yellow; g - green.
+>
+stain byybb
+abbot
+abdat
+ablet
+...
+wheat
+wreat
+zayat
+zakat
+538
+Enter your guessed word, followed by the colors returned for your guess.
+Valid colors are b - black; y - yellow; g - green.
+>
+vocal bybgy
+bloat
+float
+gloat
+lutao
+oblat
+ploat
+talao
+7
+Enter your guessed word, followed by the colors returned for your guess.
+Valid colors are b - black; y - yellow; g - green.
+>
+oblat yyygg
+bloat
+1
 ```
