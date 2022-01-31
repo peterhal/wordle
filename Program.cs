ccb90786d307c5868422c143b5ba650efce0179a
Peter Hallam <peter@peterhallam.com>
2022-01-30 23:57:31 -0800

    Better yellow handling

--- Program.cs
+++ Program.cs
@@ -12,6 +12,13 @@ namespace wordle {
             return Enumerable.Zip(template, value).All(pair => pair.First == pair.Second || pair.First == '_');
         }
 
+        static bool MatchesCompiledTemplate(List<Predicate<char>> template, string value) {
+            if (template.Count != value.Length) {
+                return false;
+            }
+            return Enumerable.Zip(template, value).All(pair => pair.First(pair.Second));
+        }
+
         static bool ContainsAny(HashSet<char> values, string value) {
             return value.Any(ch => values.Contains(ch));
         }
@@ -19,6 +26,34 @@ namespace wordle {
         static bool ContainsAll(List<char> values, string value) {
             return values.All(value.Contains);
         }
+
+        // Template syntax:
+        //    _ = any character
+        //    [<chars>] = not any of <chars>
+        //    x = must be char x
+        static List<Predicate<char>> compileTemplate(string template) {
+            var result = new List<Predicate<char>>();
+            for (var index = 0; index < template.Length; index++) {
+                var ch = template[index];
+                Predicate<char> predicate;
+                if (ch == '_') {
+                    predicate = c => true;
+                } else if (ch == '[') {
+                    index++;
+                    var excludes = new List<char>();
+                    while (template[index] != ']') {
+                        excludes.Add(template[index]);
+                        index++;
+                    }
+                    predicate = c => !excludes.Contains(c);
+                } else {
+                    predicate = c => c == ch;
+                }
+                result.Add(predicate);
+            }
+            return result;
+        }
+
         static int Main(string[] args) {
             if (args.Length != 4) {
                 Console.Error.WriteLine("Usage: wordle <dictionary> <template> <excludes> <includes>");
@@ -27,11 +62,14 @@ namespace wordle {
 
             var words = File.ReadAllLines(args[0]);
             var template = args[1];
+            var compiledTemplate = compileTemplate(template);
             var excludes = new HashSet<char>(args[2]);
             var includes = args.Length >= 4 ? new List<char>(args[3]) : new List<char>();
-            foreach (var word in words.Where(word => MatchesTemplate(template, word) && !ContainsAny(excludes, word) && ContainsAll(includes, word))) {
+            var results = words.Where(word => MatchesCompiledTemplate(compiledTemplate, word) && !ContainsAny(excludes, word) && ContainsAll(includes, word)).ToArray();
+            foreach (var word in results) {
                 Console.WriteLine(word);
             }
+            Console.WriteLine(results.Length);
             return 0;
         }
     }
